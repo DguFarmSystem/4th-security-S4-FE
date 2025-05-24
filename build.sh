fb81abcf5db272181b99793fab3f2fdf9fb06c77
MINJU <mjryu00211@gmail.com>
2025-05-24 18:21:41 +0900

    Fix: 레포 연동 문제 해결

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./4th-security-s4-fe/* ./output
-cp -R ./output ./4th-security-s4-fe/
+cp -R ./4th-security-S4-FE/* ./output
+cp -R ./output ./4th-security-S4-FE/
