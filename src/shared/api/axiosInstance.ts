23a16e0c058ed0088f7591f2a4b89c85f70d204b
MINJU <mjryu00211@gmail.com>
2025-05-26 17:38:09 +0900

    Chore: changed api address

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://fellow-myrilla-dohyunk58-781ebbd2.koyeb.app',
+  baseURL: 'https://memekly.koyeb.app',
   timeout: 5000,
 });
