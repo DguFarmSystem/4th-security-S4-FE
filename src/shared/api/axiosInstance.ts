f93d1468e42655a98b4d668241e9c9a1e4bf6353
MINJU <mjryu00211@gmail.com>
2025-05-26 20:03:35 +0900

    chore: bypassing proxy

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -2,5 +2,9 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'https://memekly.koyeb.app',
-  timeout: 5000,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  withCredentials: false,
 });
